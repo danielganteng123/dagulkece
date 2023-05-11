384cc17c9a4d0bba4684c5bec4fcedfdc6dfeed6
Daniel Stephen Gultom <danielgultom306@gmail.com>
2023-05-12 01:46:34 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Ini paragraph Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti fugit nesciunt repudiandae, ratione temporibus asperiores modi saepe quae commodi quo magni praesentium. Doloribus libero tenetur, id assumenda sed asperiores quidem?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
